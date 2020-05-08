3bf5781af1da30382cb3da1a21b08c6117ef1409
crantz007 <youskant@gmail.com>
2020-05-07 19:49:44 -0500

    gcp deployment

---
+++ travel_wishlist/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views ,admin_views
+
+urlpatterns = [
+    path('', views.place_list, name='place_list'),
+    path('visited', views.places_visited, name='places_visited'),
+    path('place/<int:place_pk>/was_visited', views.place_was_visited, name='place_was_visited'),
+    path('place/<int:place_pk>', views.place_details, name='place_details'),
+    path('place/<int:place_pk>/delete', views.delete_place, name='delete_place'),
+    path('catfact', admin_views.get_cat_fact, name='admin_get_cat_fact')
+]
